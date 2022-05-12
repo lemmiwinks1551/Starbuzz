54d31332c165f61204315703b2a221823cb5dc67
lemmiwinks1551 <ananevka@mail.ru>
2022-05-12 23:06:36 +0300

    first step

---
+++ app/src/main/java/com/example/starbuzzjava/Drink.java
@@ -0,0 +1,38 @@
+package com.example.starbuzzjava;
+
+import androidx.annotation.NonNull;
+
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    private Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public static final Drink[] drinks = {
+            new Drink("Latte", "A couple of espresso shots with steamed milk", R.drawable.latte),
+            new Drink("Cappuccino", "\"Espresso, hot milk, and a steamed milk foam", R.drawable.cappuccino),
+            new Drink("Filter", "\"Highest quality beans roasted and brewed fresh", R.drawable.filter)
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    @NonNull
+    public String toString() {
+        return name;
+    }
+}
