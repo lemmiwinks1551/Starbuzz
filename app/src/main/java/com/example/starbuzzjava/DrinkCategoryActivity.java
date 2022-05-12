69cc021ee600a5bd26cf8f63383db60d97afacc9
lemmiwinks1551 <ananevka@mail.ru>
2022-05-12 23:44:01 +0300

    first step

---
+++ app/src/main/java/com/example/starbuzzjava/DrinkCategoryActivity.java
@@ -0,0 +1,5 @@
+package com.example.starbuzzjava;
+
+public class DrinkCategoryActivity {
+
+}
--- app/src/main/java/com/example/starbuzzjava/TopLevelActivity.java
+++ app/src/main/java/com/example/starbuzzjava/TopLevelActivity.java
@@ -2,13 +2,34 @@ package com.example.starbuzzjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
+
+    /**
+     * 1. Метод onCreate() создает объект onItemClickListener и связывает его с объектом ListView.
+     * 2. Когда пользователь щелкает на варианте из спискового представления, вызывается метод onItemClick() слушателя onItemClickListener.
+     * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                }
+            }
+        };
+        //Добавить слушателя к списковому представлению
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
