04de7e088650f88024b214d6541ea04140c4be65
lemmiwinks1551 <ananevka@mail.ru>
2022-05-13 23:40:02 +0300

    Drinks

--- app/src/main/java/com/example/starbuzzjava/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/starbuzzjava/DrinkCategoryActivity.java
@@ -1,5 +1,27 @@
 package com.example.starbuzzjava;
 
-public class DrinkCategoryActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+/**
+ * 1. Когда пользователь выбирает команду Drinks, открывается активность DrinkCategoryActivity.
+ * 2. DrinkCategoryActivity создает ArrayAdapter<Drink> — адаптер массива для массивов, содержащих объекты Drink.
+ * 3. Источником данных адаптера массива является массив drinks из класса Drink.
+ * 4. DrinkCategoryActivity приказывает ListView использовать адаптер массива, вызывая метод setAdapter().
+ */
+
+public class DrinkCategoryActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
+
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+
+        listDrinks.setAdapter(listAdapter);
+    }
 }
--- app/src/main/java/com/example/starbuzzjava/TopLevelActivity.java
+++ app/src/main/java/com/example/starbuzzjava/TopLevelActivity.java
@@ -25,9 +25,11 @@ public class TopLevelActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
                 if (position == 0) {
                     Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
                 }
             }
         };
+
         //Добавить слушателя к списковому представлению
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
